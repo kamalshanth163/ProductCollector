bd28e3cdf79ae9952413b1a4b26d64469e6afc61
Kamalshanth Selvarajah <kamalshanthdeveloper163@gmail.com>
2022-04-20 16:11:08 +0530

    Complete admin dashboard view

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../App.css'
+import '../../styles/Dashboard.css'
 import NavBar from '../NavBar';
 
 function Dashboard() {
@@ -8,7 +8,46 @@ function Dashboard() {
     <div>
       <NavBar />
       <div className="dashboard-page row">
-        <h1>Dashboard</h1>
+        {/* <h1>Dashboard</h1> */}
+        
+        <div className="container">
+
+          <div className="row analytics">
+            <h1>Analytics</h1>
+            <div className="col box">
+              <h2>Products</h2>
+              <span>16</span>
+            </div>
+            <div className="col box">
+              <h2>Orders</h2>
+              <span>16</span>
+            </div>
+            <div className="col box">
+              <h2>Collectors</h2>
+              <span>16</span>
+            </div>
+            <div className="col box">
+              <h2>Holders</h2>
+              <span>16</span>
+            </div>
+          </div>
+
+          <div className="row reports">
+            <h1>Reports</h1>
+            <div className="col box">
+              <span>Generate Products Report</span>
+            </div>
+            <div className="col box">
+              <span>Generate Orders Report</span>
+            </div>
+            <div className="col box">
+              <span>Generate Collectors Report</span>
+            </div>
+            <div className="col box">
+              <span>Generate Holders Report</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
